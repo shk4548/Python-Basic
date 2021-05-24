09856b4729e7aba8ca0e9d75aa2ec43c0283f4f7
ksh <shk4548@gmail.com>
2021-05-24 14:08:35 +0900

    글로벌 Scope, 내장 Scope

--- adv_fuction_scope.py
+++ adv_fuction_scope.py
@@ -11,3 +11,18 @@ def scope_func2(a):
 print(scope_func(10))
 print(scope_func2(10))
 print("global x:", x)       # scope_func2에서 x가 치한되었지만 global x는 변경되지 않음
+
+g = 1 # 글로벌 변수
+
+def scope_func3(a):
+    global g
+    g = 3
+    return a + g
+
+print(scope_func3(10))
+print("global g:", g)
+
+# 글로벌 영역의 확인
+print(dir())    # globals
+# 내장 영역의 확인
+print("내장영역:", dir('__builtins__'))
